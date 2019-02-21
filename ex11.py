0259a4661df4bb7db520c04bafa728448fa4a44e
danimrprofe <danimrprofe@gmail.com>
2019-02-21 19:26:18 +0100

    hasta el 14

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print ("How old are you?"),
+age = input()
+print ("How tall are you?"),
+height = input()
+print ("How mucho do you weight?"),
+weight = input()
+
+print ("So you are %r old, %r tall and %r heavy" % (age, height, weight))
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Daniel'
+my_age = 34 # not a lie
+my_height = 174 # centimetros
+my_weight = 70 # kg
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print ("Let's talok about %s" % my_name)
+print ("He's actually %d cm de alto" % my_height)
+print ("He is %d kg heavy" % my_weight)
+print ("Actually that's not too heavy")
+print ("He's got %s eyes and %s hair" % (my_eyes, my_hair) )
+print ("His teeth are usually %s depending on the coffee" % my_teeth)
+
+print ("If I add, %d, %d and %d I get %d" % (my_age, my_height, my_weight, my_age
++ my_height + my_weight))
